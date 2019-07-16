84dc20e0ef5053a7bf777afc72e6c398dcd4798c
Nuno Domingues <nunodominguesmobiledeveloper@gmail.com>
2019-07-16 22:39:37 +0100

    Creation of Custom Button Class

---
+++ CustomButton/CustomButton/CustomButton/CustomButton.swift
@@ -0,0 +1,53 @@
+//
+//  CustomButton.swift
+//  CustomButton
+//
+//  Created by Nuno Domingues on 16/07/2019.
+//  Copyright © 2019 NDD. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class CustomButton: UIButton {
+    
+    //Default Values
+    var borderColor: UIColor = .gray
+    var borderWidth: CGFloat = 2
+    var buttonBackgroundColor: UIColor = UIColor(red: 49/255, green: 126/255, blue: 238/255, alpha: 1)
+    var buttonTextColor: UIColor = .white
+    var buttonTitleFont: UIFont = UIFont.systemFont(ofSize: 16, weight: .bold)
+    
+    //Init programatically
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButton()
+    }
+    
+    //Init Programatically
+    init(withTitle title: String, frame: CGRect) {
+        super.init(frame: frame)
+        self.setTitle(title, for: .normal)
+        setupButton()
+    }
+    
+    //Init with Storyboard
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupButton()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setupButton()
+    }
+    
+    //Assign default Values into Button
+    func setupButton() {
+        layer.cornerRadius = self.frame.height / 2
+        layer.borderColor = borderColor.cgColor
+        layer.borderWidth = borderWidth
+        backgroundColor = buttonBackgroundColor
+        setTitleColor(buttonTextColor, for: .normal)
+        titleLabel?.font = buttonTitleFont
+    }
+}
